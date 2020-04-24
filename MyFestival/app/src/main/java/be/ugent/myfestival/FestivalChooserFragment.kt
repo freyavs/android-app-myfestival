e7f35e049c87e472b5d7a20e2ec076be6f0568c8
fvspeybr <Freya.VanSpeybroeck@UGent.be>
2020-04-24 11:07:39 +0200

    pijltje weg

--- MyFestival/app/src/main/java/be/ugent/myfestival/FestivalChooserFragment.kt
+++ MyFestival/app/src/main/java/be/ugent/myfestival/FestivalChooserFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -52,6 +53,11 @@ class FestivalChooserFragment : Fragment() {
         editor?.apply()
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
     }
 
     private fun handleItemClick(festivalChooser: FestivalChooser) {
