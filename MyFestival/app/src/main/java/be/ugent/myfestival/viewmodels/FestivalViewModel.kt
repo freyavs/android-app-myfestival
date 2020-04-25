77cdbaa449e535493d548b6766bd91f61ca8ef06
Thor Dossche <thor.dossche@ugent.be>
2020-04-25 13:11:42 +0200

    verschijnen buttons, inorde maar andere referentie nodig

--- MyFestival/app/src/main/java/be/ugent/myfestival/viewmodels/FestivalViewModel.kt
+++ MyFestival/app/src/main/java/be/ugent/myfestival/viewmodels/FestivalViewModel.kt
@@ -3,20 +3,27 @@ package be.ugent.myfestival.viewmodels
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import be.ugent.myfestival.data.FestivalRepository
 import be.ugent.myfestival.databinding.FestivalChooserFragmentBindingImpl
+import com.bumptech.glide.request.target.Target
 import be.ugent.myfestival.models.Dish
 import be.ugent.myfestival.models.FestivalChooser
 import be.ugent.myfestival.models.FoodStand
 import be.ugent.myfestival.utilities.GlideApp
+import be.ugent.myfestival.utilities.InjectorUtils
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import java.io.File
 
 
@@ -109,18 +116,37 @@ class FestivalViewModel(private val festivalRepo : FestivalRepository) : ViewMod
         Log.v("welcome_string", value.isEmpty().toString())
         Log.v("welcome_string", value.toString())
         when(value.isNullOrEmpty()) {
-            true -> View.VISIBLE
+            true -> View.GONE
             else -> View.INVISIBLE
         }
     }
 
+    fun getReady(): LiveData<Int> = Transformations.map(festivalRepo.getFestivalName()){ value ->
+        when(value.isNullOrEmpty()) {
+            false -> View.GONE
+            else -> View.VISIBLE
+        }
+    }
 
     companion object {
         //databinding met glide
         @JvmStatic
         @BindingAdapter("imageUrl")
         fun loadImage(view: ImageView, url: String?) {
-            GlideApp.with(view.context).load(url).into(view)
+            GlideApp.with(view.context).load(url)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                        return false
+                    }
+                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                        Log.d("myL", "OnResourceReady")
+
+                        //do something when picture already loaded
+                        return false
+                    }
+                })
+                .into(view)
+
         }
 
     }
