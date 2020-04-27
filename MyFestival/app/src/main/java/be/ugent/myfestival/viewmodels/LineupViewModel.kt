05656210282da51549f1d5923a32e7ee017b763a
Thor Dossche <thor.dossche@ugent.be>
2020-04-27 23:30:36 +0200

    tests

--- MyFestival/app/src/main/java/be/ugent/myfestival/viewmodels/LineupViewModel.kt
+++ MyFestival/app/src/main/java/be/ugent/myfestival/viewmodels/LineupViewModel.kt
@@ -7,19 +7,11 @@ import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import be.ugent.myfestival.data.FestivalRepository
 import be.ugent.myfestival.models.Stage
-import java.text.SimpleDateFormat
 import java.time.LocalDate
-import java.time.ZoneId
-import java.util.*
 
 class LineupViewModel(private val festivalRepo : FestivalRepository) : ViewModel() {
 
-    val TAG = "myFestivalTag"
-
     var currentDay: MutableLiveData<LocalDate> = MutableLiveData(LocalDate.now())
-
-
-    //TODO: nog aanduiden dat swipen mogelijk is of niet adhv livedata en "<" en ">" mss (en dit ook zeker testen in de unit tests)
 
     fun getAllDaysSorted() : LiveData<List<LocalDate>> = Transformations.map(festivalRepo.getLineup()) {stages ->
         val concerts = stages.flatMap{ it.concerts }
@@ -30,8 +22,10 @@ class LineupViewModel(private val festivalRepo : FestivalRepository) : ViewModel
         val list = mutableListOf<Stage>()
         for (stage in stages){
             var concerts = stage.concerts.filter{it.start.toLocalDate() == day}
-            concerts = concerts.sortedBy { concert -> concert.start }
-            list.add(Stage(stage.id, stage.name, concerts))
+            if (!concerts.isEmpty()){
+                concerts = concerts.sortedBy { concert -> concert.start }
+                list.add(Stage(stage.id, stage.name, concerts))
+            }
         }
         list
     }
@@ -39,16 +33,12 @@ class LineupViewModel(private val festivalRepo : FestivalRepository) : ViewModel
     fun getToday() = LocalDate.now()
 
     fun clickedDay(day: LocalDate) {
-        if (currentDay.value!! !== day) {
+        if (currentDay.value !== day) {
             currentDay.postValue(day)
         }
     }
 
     fun getCurrentStages() : LiveData<List<Stage>> =  Transformations.switchMap(currentDay) { day ->
-        Log.d(TAG, "Get current stages: " + day.toString())
-        Transformations.map(getStages(day)) {
-            stages -> stages
-        }
+        getStages(day)
     }
-
 }
