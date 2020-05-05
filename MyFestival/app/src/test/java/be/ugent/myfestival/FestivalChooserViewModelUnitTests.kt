19b7b70800cff4b071ab0af4becd8dffca40b6a5
wfmeersm <Ward.Meersman@UGent.be>
2020-05-05 21:19:22 +0200

    5 testen zijn af

--- MyFestival/app/src/test/java/be/ugent/myfestival/FestivalChooserViewModelUnitTests.kt
+++ MyFestival/app/src/test/java/be/ugent/myfestival/FestivalChooserViewModelUnitTests.kt
@@ -2,12 +2,19 @@ package be.ugent.myfestival
 
 import android.util.Log
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import be.ugent.myfestival.data.FestivalRepository
+import be.ugent.myfestival.models.FestivalChooser
+import be.ugent.myfestival.viewmodels.FestivalChooserViewModel
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.MutableData
 import com.google.firebase.storage.StorageReference
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import org.junit.Before
 import org.junit.Rule
@@ -17,35 +24,55 @@ class FestivalChooserViewModelUnitTests {
     @get:Rule
     @Suppress("unused")
     var instantTaskExecutorRule = InstantTaskExecutorRule()
+    val festival1 = FestivalChooser("f1", "Festival1", "")
+    val festival2 = FestivalChooser("f2", "Festival2", "")
+    val festival3 = FestivalChooser("f3", "AnotherName", "")
 
-    private lateinit var repository: FestivalRepository
+    val festivals: List<FestivalChooser> = listOf(festival1,festival2,festival3)
 
-    private lateinit var database: FirebaseDatabase
-    private lateinit var storage: StorageReference
+    private lateinit var repo: FestivalRepository
+    private lateinit var festivalChooserViewModel: FestivalChooserViewModel
 
-    private lateinit var reference: DatabaseReference
-    private lateinit var childReference: DatabaseReference
 
     @Before
     fun setup() {
-        database = mock()
-        storage = mock()
-        reference = mock()
-        childReference = mock()
-
-        whenever(database.getReference(any())).thenReturn(reference)
-        whenever(database.getReference()).thenReturn(reference)
-        whenever(reference.child(any())).thenReturn(childReference)
-        whenever(childReference.orderByKey()).thenReturn(childReference)
-
-        repository = FestivalRepository(database,storage)
-    }
-    fun fillRepository(repo: FestivalRepository){
-        repo.festivalList.value = mutableListOf(mock())
+        repo = mock()
+        whenever(repo.getFestivals()).thenReturn(MutableLiveData((festivals)))
+        festivalChooserViewModel = FestivalChooserViewModel(repo)
     }
     @Test
-    fun test(){
-        fillRepository(repository)
-        println(repository.festivalList.value)
+    fun testNoSearch(){
+        val mockObserver = mock<Observer<List<FestivalChooser>>>()
+        festivalChooserViewModel.getFestivals().observeForever(mockObserver)
+        festivalChooserViewModel.changeSearchValue("")
+        verify(mockObserver).onChanged(festivals)
+    }
+    @Test
+    fun testNameFestival(){
+        val mockObserver = mock<Observer<List<FestivalChooser>>>()
+        festivalChooserViewModel.getFestivals().observeForever(mockObserver)
+        festivalChooserViewModel.changeSearchValue("Festival")
+        verify(mockObserver).onChanged(listOf(festival1,festival2))
+    }
+    @Test
+    fun testFestival1(){
+        val mockObserver = mock<Observer<List<FestivalChooser>>>()
+        festivalChooserViewModel.getFestivals().observeForever(mockObserver)
+        festivalChooserViewModel.changeSearchValue("Festival1")
+        verify(mockObserver).onChanged(listOf(festival1))
+    }
+    @Test
+    fun testFestival2(){
+        val mockObserver = mock<Observer<List<FestivalChooser>>>()
+        festivalChooserViewModel.getFestivals().observeForever(mockObserver)
+        festivalChooserViewModel.changeSearchValue("Festival2")
+        verify(mockObserver).onChanged(listOf(festival2))
+    }
+    @Test
+    fun testAnotherName(){
+        val mockObserver = mock<Observer<List<FestivalChooser>>>()
+        festivalChooserViewModel.getFestivals().observeForever(mockObserver)
+        festivalChooserViewModel.changeSearchValue("AnotherName")
+        verify(mockObserver).onChanged(listOf(festival3))
     }
 }
