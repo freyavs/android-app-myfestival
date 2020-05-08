28ce1a3ab9b3247ae84c812f37f4ad82355bd015
fvspeybr <freya.vanspeybroeck@ugent.be>
2020-05-08 17:51:09 +0200

    home layout

--- MyFestival/app/src/main/java/be/ugent/myfestival/viewmodels/FestivalViewModel.kt
+++ MyFestival/app/src/main/java/be/ugent/myfestival/viewmodels/FestivalViewModel.kt
@@ -18,7 +18,7 @@ class  FestivalViewModel(private val festivalRepo : FestivalRepository) : ViewMo
 
     fun getWelcomeString(): LiveData<String> =
         Transformations.map(festivalRepo.getFestivalName()) { value ->
-            "Welkom bij $value"
+            "$value"
         }
 
     fun getFestivalName() = festivalRepo.getFestivalName()
